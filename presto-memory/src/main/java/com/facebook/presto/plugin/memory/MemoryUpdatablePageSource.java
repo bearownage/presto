d7a3fd37b6dfacd4c6180995988f85ad98196fe5
bearownage <adam.askari100@gmail.com>
2024-04-24 16:13:29 -0700

    init some page source stuff

--- presto-memory/src/main/java/com/facebook/presto/plugin/memory/MemoryUpdatablePageSource.java
+++ presto-memory/src/main/java/com/facebook/presto/plugin/memory/MemoryUpdatablePageSource.java
@@ -14,28 +14,113 @@
 package com.facebook.presto.plugin.memory;
 
 import com.facebook.presto.common.Page;
+import com.facebook.presto.common.Utils;
 import com.facebook.presto.common.block.Block;
+import com.facebook.presto.common.block.RunLengthEncodedBlock;
+import com.facebook.presto.common.type.TimeType;
+import com.facebook.presto.common.type.TimestampType;
+import com.facebook.presto.common.type.Type;
+import com.facebook.presto.spi.ConnectorPageSource;
+import com.facebook.presto.spi.PrestoException;
 import com.facebook.presto.spi.UpdatablePageSource;
 import io.airlift.slice.Slice;
 
 import java.io.IOException;
+import java.io.UncheckedIOException;
 import java.util.Collection;
 import java.util.List;
+import java.util.Map;
 import java.util.concurrent.CompletableFuture;
+
+import static com.google.common.base.Throwables.throwIfInstanceOf;
+import static java.util.Objects.requireNonNull;
+import static java.util.concurrent.TimeUnit.MICROSECONDS;
+import static java.util.concurrent.TimeUnit.MILLISECONDS;
 
 public class MemoryUpdatablePageSource
         implements UpdatablePageSource
 {
-    @Override
-    public void deleteRows(Block rowIds)
+    private final Block[] prefilledBlocks;
+    private final int[] delegateIndexes;
+    private final ConnectorPageSource delegate;
+
+    public MemoryUpdatablePageSource(
+            List<MemoryColumnHandle> columns,
+            Map<Integer, Object> metadataValues,
+            ConnectorPageSource delegate)
     {
-        UpdatablePageSource.super.deleteRows(rowIds);
+        int size = requireNonNull(columns, "columns is null").size();
+        this.delegate = requireNonNull(delegate, "delegate is null");
+
+
+        prefilledBlocks = new Block[size];
+        delegateIndexes = new int[size];
+
+        int outputIndex = 0;
+        int delegateIndex = 0;
+        for (MemoryColumnHandle column : columns) {
+                delegateIndexes[outputIndex] = delegateIndex;
+                delegateIndex++;
+            outputIndex++;
+        }
     }
 
     @Override
-    public void updateRows(Page page, List<Integer> columnValueAndRowIdChannels)
+    public long getCompletedBytes()
     {
-        UpdatablePageSource.super.updateRows(page, columnValueAndRowIdChannels);
+        return delegate.getCompletedBytes();
+    }
+
+    @Override
+    public long getCompletedPositions()
+    {
+        return delegate.getCompletedPositions();
+    }
+
+    @Override
+    public long getReadTimeNanos()
+    {
+        return delegate.getReadTimeNanos();
+    }
+
+    @Override
+    public boolean isFinished()
+    {
+        return delegate.isFinished();
+    }
+
+    @Override
+    public Page getNextPage()
+    {
+        try {
+            Page dataPage = delegate.getNextPage();
+            if (dataPage == null) {
+                return null;
+            }
+
+            int batchSize = dataPage.getPositionCount();
+            Block[] blocks = new Block[prefilledBlocks.length];
+            for (int i = 0; i < prefilledBlocks.length; i++) {
+                if (prefilledBlocks[i] != null) {
+                    blocks[i] = new RunLengthEncodedBlock(prefilledBlocks[i], batchSize);
+                }
+                else {
+                    blocks[i] = dataPage.getBlock(delegateIndexes[i]);
+                }
+            }
+            return new Page(batchSize, blocks);
+        }
+        catch (RuntimeException e) {
+            closeWithSuppression(e);
+            throwIfInstanceOf(e, PrestoException.class);
+            throw new PrestoException(null, e);
+        }
+    }
+
+    @Override
+    public void deleteRows(Block rowIds)
+    {
+
     }
 
     @Override
@@ -47,47 +132,53 @@ public class MemoryUpdatablePageSource
     @Override
     public void abort()
     {
-        UpdatablePageSource.super.abort();
+
     }
 
     @Override
-    public long getCompletedBytes()
+    public void close()
     {
-        return 0;
+        try {
+            delegate.close();
+        }
+        catch (IOException e) {
+            throw new UncheckedIOException(e);
+        }
     }
 
     @Override
-    public long getCompletedPositions()
+    public String toString()
     {
-        return 0;
-    }
-
-    @Override
-    public long getReadTimeNanos()
-    {
-        return 0;
-    }
-
-    @Override
-    public boolean isFinished()
-    {
-        return false;
-    }
-
-    @Override
-    public Page getNextPage()
-    {
-        return null;
+        return delegate.toString();
     }
 
     @Override
     public long getSystemMemoryUsage()
     {
-        return 0;
+        long totalMemUsage = delegate.getSystemMemoryUsage();
+
+        return totalMemUsage;
     }
 
-    @Override
-    public void close() throws IOException
+    protected void closeWithSuppression(Throwable throwable)
     {
+        requireNonNull(throwable, "throwable is null");
+        try {
+            close();
+        }
+        catch (RuntimeException e) {
+            // Self-suppression not permitted
+            if (throwable != e) {
+                throwable.addSuppressed(e);
+            }
+        }
+    }
+
+    private Block nativeValueToBlock(Type type, Object prefilledValue)
+    {
+        if (prefilledValue != null && (type instanceof TimestampType && ((TimestampType) type).getPrecision() == MILLISECONDS || type instanceof TimeType)) {
+            return Utils.nativeValueToBlock(type, MICROSECONDS.toMillis((long) prefilledValue));
+        }
+        return Utils.nativeValueToBlock(type, prefilledValue);
     }
 }
